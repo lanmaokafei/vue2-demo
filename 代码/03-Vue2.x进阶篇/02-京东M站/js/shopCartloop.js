888b07d8634087c880e14662d3ef948101fb2e85
xyj <roheike@qq.com>
2018-05-01 22:55:42 +0800

    成功使用axios替换vue-resource

--- 代码/03-Vue2.x进阶篇/02-京东M站/js/shopCartloop.js
+++ 代码/03-Vue2.x进阶篇/02-京东M站/js/shopCartloop.js
@@ -1,23 +1,25 @@
 new Vue({
-    el: "#app",
-    data: {
+    el:'#app',
+    data:{
         // 购物车中的数据
-        shopListArr:[],
+        shopListArr: []
     },
     // 组件加载完毕,请求网络数据,业务处理
     mounted(){
         // 请求本地的数据
-        this.getLocalData();
+        this.getLocalData()
     },
     methods:{
         // 1. 请求本地数据
         getLocalData(){
-            axios.get('data/cart.json', {
+
+            // 使用axios
+            return axios.get('data/cart.json', {
                 params: {
                   ID: 12345
                 }
               })
-              .then(function (response) {
+              .then((response) => {
                 console.log(response.data);
                 const res = response.data;
                 if(res){
@@ -29,6 +31,19 @@ new Vue({
                 console.log(error);
                 alert('请求数据失败!');
               });
+
+            // 使用vue-resource
+            // this.$http.get('data/cart.json').then(response => {
+            //     this.someData = response.body;
+            //     console.log(this.someData);
+            //     const res = this.someData;
+            //     if(res){
+            //         this.shopListArr = res.allShops.shopList;
+            //         console.log(this.shopListArr);
+            //     }
+            // },response =>{
+            //
+            // });
         }
     }
 });
